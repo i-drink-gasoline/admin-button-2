7e878a7af8e051a3f35c8671f2328c33ee278e41
i-drink-gasoline <>
2025-04-09 16:25:32 +0200

    Replace message prefix with message formatting

--- src/adminbutton2/ui/AdminDialog.java
+++ src/adminbutton2/ui/AdminDialog.java
@@ -12,31 +12,27 @@ import mindustry.ui.dialogs.BaseDialog;
 import adminbutton2.Secret;
 
 public class AdminDialog extends BaseDialog {
-    private String messagePrefix = "";
-    private boolean messagePrefixEnabled = false;
     private String secretMessage = "";
     private char icon = Iconc.admin;
     private TextButton chooserButton = new TextButton(String.valueOf(icon));
+    TextField formatField = new TextField();
 
     public AdminDialog() {
         super("@adminbutton2.admindialog.title");
         BaseDialog iconChooser = iconChooserDialog();
+        formatField.setMessageText("/t {}");
         addCloseButton();
         cont.table(t -> {
             t.table(t2 -> {
                 t2.add(chooserButton).width(50).height(50).get().changed(() -> iconChooser.show());
-                TextField prefix = t2.field("", text -> messagePrefix = text).get();
-                prefix.setMessageText("/t");
-                prefix.setDisabled(!messagePrefixEnabled);
-                t2.check("@adminbutton2.admindialog.message_prefix", messagePrefixEnabled, enabled -> prefix.setDisabled(!(messagePrefixEnabled = enabled))).padLeft(4).get();
-                t2.getCells().swap(1, 2);
+                t2.add(formatField).width(350);
             });;
             t.row();
             t.field("", text -> secretMessage = text).width(400).height(50).get().setMessageText("@adminbutton2.admindialog.secret_message");
             t.row();
             t.table(t2 -> {
                 t2.button("@adminbutton2.admindialog.send_secret_message", () -> sendMessage(Secret.generateSecretMessage(icon, secretMessage))).width(200).height(50);
-                t2.button("@adminbutton2.admindialog.send_random_message", () -> sendMessage(Secret.generateRandomMessage(icon, Vars.maxTextLength - messagePrefix.length()))).width(200).height(50);
+                t2.button("@adminbutton2.admindialog.send_random_message", () -> sendMessage(Secret.generateRandomMessage(icon, Vars.maxTextLength - formatField.getText().length()))).width(200).height(50);
             }).marginTop(8);
             t.row();
             t.button("@adminbutton2.message_list.title", () -> adminbutton2.AdminVars.messages.show()).width(200).height(50).left();
@@ -44,7 +40,8 @@ public class AdminDialog extends BaseDialog {
     }
 
     private void sendMessage(String message) {
-        String msg = (messagePrefixEnabled == true ? messagePrefix : "") + message;
+        if (!formatField.getText().contains("{}")) formatField.setText("{}");
+        String msg = formatField.getText().replaceFirst("\\{\\}", message);
         if (msg.length() > Vars.maxTextLength) {
             Vars.ui.showErrorMessage("@adminbutton2.admindialog.message_above_limit");
             return;
