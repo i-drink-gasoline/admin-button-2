310dea2f740587cca5db5325bac95c1eda8780c3
i-drink-gasoline <>
2025-04-10 18:36:08 +0200

    Fix a mistake in the build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 [ -n "$MINDUSTRYJAR" ] || { echo '$MINDUSTRYJAR is not set' && exit 1; }
 
 mkdir -p build
-javac --release 8 -d build $(find src -name '*.java') -classpath /utmp/Mindustry.jar
+javac --release 8 -d build $(find src -name '*.java') -classpath "$MINDUSTRYJAR"
 jar -cf build/adminbutton2.jar mod.json icon.png bundles -C build adminbutton2
 "$ANDROID_HOME/build-tools/36.0.0-rc3/d8" --min-api 14 build/adminbutton2.jar --classpath "$MINDUSTRYJAR" --classpath "$ANDROID_HOME/platforms/android-16/android.jar" --output build/
 jar -uf build/adminbutton2.jar -C build classes.dex
