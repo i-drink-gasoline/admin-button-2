83957c07881326b8c0470528ca728935e97d81fa
i-drink-gasoline <>
2025-04-23 19:24:41 +0200

    Fix admin language not loading

--- src/adminbutton2/AdminButton.java
+++ src/adminbutton2/AdminButton.java
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 package adminbutton2;
 
-import arc.Events;
 import mindustry.Vars;
-import mindustry.game.EventType;
 import mindustry.gen.Icon;
 import mindustry.mod.Mod;
 import mindustry.ui.Styles;
@@ -14,9 +12,7 @@ import adminbutton2.ui.AdminChatFragment;
 public class AdminButton extends Mod {
     @Override
         public void init() {
-        Events.run(EventType.ContentInitEvent.class, () -> {
-            AdminVars.loadLanguage();
-        });
+        AdminVars.loadLanguage();
         AdminVars.init();
         Vars.ui.hudGroup.fill(t -> {
             t.button(Icon.admin, Styles.cleari, () -> {
