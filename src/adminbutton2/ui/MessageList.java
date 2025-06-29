b95168339628a943b0cb473302b728343701bdc0
i-drink-gasoline <>
2025-06-29 11:46:28 +0200

    Add search field to message list

--- src/adminbutton2/ui/MessageList.java
+++ src/adminbutton2/ui/MessageList.java
@@ -4,21 +4,44 @@ package adminbutton2.ui;
 import arc.Core;
 import arc.scene.ui.ScrollPane;
 import arc.scene.ui.layout.Table;
+import arc.struct.Seq;
 import mindustry.gen.Icon;
 import mindustry.ui.dialogs.BaseDialog;
 
 public class MessageList extends BaseDialog {
     private Table messageTable;
+    private Seq<String> messages = new Seq<String>();
+    private String text = "";
     public MessageList() {
         super("@adminbutton2.message_list.title");
         addCloseButton();
+        cont.top();
+        cont.table(t -> {
+            t.image(Icon.zoom);
+            t.field(text, a -> {
+                text = a;
+                rebuild();
+            }).growX().row();
+        }).fillX().row();
         messageTable = new Table();
         ScrollPane pane = new ScrollPane(messageTable);
         cont.pane(pane).padLeft(80).padRight(80).growX();
     }
 
     public void addMessage(String message) {
-        messageTable.button(Icon.copy, () -> Core.app.setClipboardText(message));
-        messageTable.add(message).growX().padLeft(8).row();
+        addMessage(message, true);
+    }
+
+    public void addMessage(String message, boolean add) {
+        if (add) messages.add(message);
+        if (message.contains(text)) {
+            messageTable.button(Icon.copy, () -> Core.app.setClipboardText(message));
+            messageTable.add(message).growX().padLeft(8).row();
+        }
+    }
+
+    private void rebuild() {
+        messageTable.clearChildren();
+        messages.each(m -> addMessage(m, false));
     }
 }
