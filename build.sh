ad5e184373a61990421b3cfc044138d107c8882b
i-drink-gasoline <>
2025-10-31 06:08:15 +0100

    Impove build script

--- build.sh
+++ build.sh
@@ -2,11 +2,28 @@
 
 set -e
 
-[ -n "$ANDROID_HOME" ] || { echo '$ANDROID_HOME is not set' && exit 1; }
-[ -n "$MINDUSTRYJAR" ] || { echo '$MINDUSTRYJAR is not set' && exit 1; }
+MINDUSTRY_VERSION=$(jq -r .minGameVersion mod.json)
+MINDUSTRY_PATH="build/Mindustry/Mindustry-v${MINDUSTRY_VERSION}.jar"
 
-mkdir -p build
-javac --release 8 -d build $(find src -name '*.java') -classpath "$MINDUSTRYJAR"
+mkdir -p "build/Mindustry"
+
+[ -e "${MINDUSTRY_PATH}" ] || {
+    if [ -n "${DOWNLOAD_MINDUSTRY}" ]; then
+        wget "https://github.com/Anuken/Mindustry/releases/download/v${MINDUSTRY_VERSION}/Mindustry.jar" -O "${MINDUSTRY_PATH}"
+    else
+        echo "${MINDUSTRY_PATH} does not exist. Set \$DOWNLOAD_MINDUSTRY envirovment variable to download it."
+        exit 1
+    fi
+}
+
+
+rm -rf "build/adminbutton2"
+javac --release 8 -d build $(find src -name '*.java') -classpath "${MINDUSTRY_PATH}"
 jar -cf build/adminbutton2.jar mod.json icon.png bundles -C build adminbutton2
-"$ANDROID_HOME/build-tools/36.0.0-rc3/d8" --min-api 14 build/adminbutton2.jar --classpath "$MINDUSTRYJAR" --classpath "$ANDROID_HOME/platforms/android-16/android.jar" --output build/
-jar -uf build/adminbutton2.jar -C build classes.dex
+
+if [ -n "${ANDROID_HOME}" ]; then
+    "$ANDROID_HOME/build-tools/36.0.0-rc3/d8" --min-api 14 build/adminbutton2.jar --classpath "${MINDUSTRY_PATH}" --classpath "${ANDROID_HOME}/platforms/android-16/android.jar" --output build/
+    jar -uf build/adminbutton2.jar -C build classes.dex
+else
+    echo '$ANDROID_HOME is not set. Skipping Android version.'
+fi
